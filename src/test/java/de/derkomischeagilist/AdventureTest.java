11b6936d9e59189be0ebd502305d1192a64dea8e
Falk Kühnel <falk@kuehnel.eu>
2021-11-21 12:24:34 +0100

    added Loop Tests

--- src/test/java/de/derkomischeagilist/AdventureTest.java
+++ src/test/java/de/derkomischeagilist/AdventureTest.java
@@ -1,2 +1,43 @@
-package de.derkomischeagilist;public class AdventureTest {
+package de.derkomischeagilist;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class AdventureTest {
+
+    private Adventure adventure;
+
+    @BeforeEach
+    void setUp() {
+        adventure = new Adventure();
+    }
+
+    @Test
+    void AdventureStartsOnTheLoo() {
+        assertThat(adventure.Begin(), containsStringIgnoringCase("you wake up on the Loo"));
+    }
+
+
+
+    @Test
+    void LookingAroundInTheLooThenICanSeeMagazines() {
+        //given i am on the loo
+        assertThat(adventure.Begin(), containsStringIgnoringCase("you wake up on the Loo"));
+        //When i look around
+        String actual = adventure.tell("look around");
+        //Then i can see magazines
+        assertThat(actual, containsStringIgnoringCase("magazines"));
+    }
+
+    @Test
+    void LookingAtMagazinesInTheLooThenICanSeeAScrumGuide() {
+        //given i am on the loo
+        assertThat(adventure.Begin(), containsStringIgnoringCase("you wake up on the Loo"));
+        //When i look around
+        String actual = adventure.tell("look at magazines");
+        //Then i can see magazines
+        assertThat(actual, containsStringIgnoringCase("scrum guide"));
+    }
 }
