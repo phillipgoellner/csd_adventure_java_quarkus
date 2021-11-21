d96f8a7589589358ac9ebe44a58e63288ca133c2
Falk Kühnel <falk@kuehnel.eu>
2021-11-21 12:21:55 +0100

    Added Adventure Loo Tests

---
+++ src/main/java/de/derkomischeagilist/Rooms/Loo.java
@@ -0,0 +1,11 @@
+package de.derkomischeagilist.Rooms;
+
+public class Loo {
+    public String Description() {
+        return "You wake up on the loo. You have no idea where or who you are.";
+    }
+
+    public String DetailedDescription() {
+        return "You see a pretty dirty door, with some nasty jokes on it. Next to you is some toilet paper, a coin and a few magazines.";
+    }
+}
---
+++ src/test/java/de/derkomischeagilist/AdventureTest.java
@@ -0,0 +1,2 @@
+package de.derkomischeagilist;public class AdventureTest {
+}
