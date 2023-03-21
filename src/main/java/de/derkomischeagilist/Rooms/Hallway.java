0c0e3403075ed3a625182dcec21109a978a4ba67
Phillip <phillip.goellner@colenet.de>
2023-03-21 11:37:58 +0100

    Implement a spooky door in the Hallway

--- src/main/java/de/derkomischeagilist/Rooms/Hallway.java
+++ src/main/java/de/derkomischeagilist/Rooms/Hallway.java
@@ -8,16 +8,22 @@ public class Hallway extends AbstractRoom {
 
     @Override
     public String getDetailedDescription() {
-        return "Welcome to the very dark hallway to hell.";
+        return "Welcome to the very dark hallway to hell. At the end you see a spooky looking door.";
     }
 
     @Override
     public String handleCommand(String command) {
-        return super.handleCommand(command);
+        switch (command) {
+            case "inspect the spooky door":
+                return "You see a rugged and sturdy steel door with cryptic symbols on it. The door is covered in cobweb and next to it is a rusty keypad. If you feel brave enough you could 'open the spooky door'?";
+            case "open the spooky door":
+                return "It seems locked and doesn't budge.";
+            default: return super.handleCommand(command);
+        }
     }
 
     @Override
     public String getHelp() {
-        return super.getHelp()+ "Try to 'look around' or 'use door to team office'. You can also 'go through door' to go to the washroom or 'use door to kitchen' to visit pizza the hut.";
+        return super.getHelp()+ "Try to 'look around' or 'use door to team office'. You can also 'go through door' to go to the washroom or 'use door to kitchen' to visit pizza the hut or maybe try to 'inspect the spooky door'.";
     }
 }
