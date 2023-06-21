c45bdeaabffaad680887b1079f2fd9ac27bdebda
rhaberstroh <rhaberstroh>
2023-06-21 12:21:34 +0200

    Change the Keypad Key for the spooky door

--- src/main/java/de/derkomischeagilist/Rooms/Hallway.java
+++ src/main/java/de/derkomischeagilist/Rooms/Hallway.java
@@ -18,7 +18,7 @@ public class Hallway extends AbstractRoom {
 
     @Override
     public String getDetailedDescription() {
-        return "Welcome to the very dark hallway to hell. You see 3 doors. One door leads to the kitchen, one to the washroom. At the end you see a spooky looking door.";
+        return "Welcome to the very dark hallway to hell. You see 3 doors. One door leads to the kitchen, one to the washroom. At the end you see a spooky looking door. One of the walls there is a dirty poster.";
     }
 
     @Override
@@ -28,6 +28,8 @@ public class Hallway extends AbstractRoom {
                 return "You see a rugged and sturdy steel door with cryptic symbols on it. Above the door is a sign saying \"EXIT\". "
                 		+ "The door is covered in cobweb and next to it is a rusty keypad. The keypad has a post-it attached to saying \"Please enter number of Scrum values.\". "
                 		+ "You could try to unlock the door with 'use keypad'.";
+            case "inspect the poster":
+                return "The mighty Scrum Values are commitment, courage, focus, openness, and respect";
             case "use keypad":
             case "use rusty keypad":
                 keypadIsActive = true;
@@ -56,6 +58,6 @@ public class Hallway extends AbstractRoom {
 
     @Override
     public String getHelp() {
-        return "Try to 'look around' or 'use door to team office'. You can also 'use door to washroom' to go to the washroom or 'use door to kitchen' to visit pizza the hut or maybe try to 'use spooky door'. If you are really interested in the poster then 'inspect the poster'" + super.getHelp();
+        return "Try to 'look around' or 'use door to team office'. You can also 'use door to washroom' to go to the washroom or 'use door to kitchen' to visit pizza the hut or maybe try to 'inspect the spooky door'. If you are really interested in the poster then 'inspect the poster'" + super.getHelp();
     }
 }
