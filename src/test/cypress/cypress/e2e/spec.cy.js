603e602126559087955336537b4c3574660fe628
Falk Kühnel <falk@kuehnel.eu>
2023-07-26 08:11:44 +0200

    test: added e2e test with network request mock

--- src/test/cypress/cypress/e2e/spec.cy.js
+++ src/test/cypress/cypress/e2e/spec.cy.js
@@ -1,8 +1,21 @@
-describe('template spec', () => {
+describe('e2e test', () => {
   it('passes', () => {
     cy.visit('/')
     cy.contains('here').click()
     cy.get('body').contains('Hello Gamers')
     cy.get('#command').type('help{enter}')
+  })
+})
+
+
+describe('e2e test with mocked server response', () => {
+  it('passes', () => {
+
+    cy.intercept('GET', '/command*', 'some txt').as('getCommand')
+
+    cy.visit('/game')
+    cy.get('#command').type('help{enter}')
+    cy.wait('@getCommand')
+    cy.get('body').contains('some txt')
   })
 })
