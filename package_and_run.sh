00e39b939450be2a226b384d41b7ccd250920efe
goellnerphillip <phillip@ubuntu01.fritz.box>
2023-11-06 10:44:49 +0100

    Prepare files for a-csd training on November 13th

--- package_and_run.sh
+++ package_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./mvnw package
-pgrep -f quarkus.http.port=4711 | xargs kill -9
-java -Dquarkus.http.port=4711 -jar target/quarkus-app/quarkus-run.jar &
+pgrep -f quarkus.http.port=80 | xargs kill -9
+java -Dquarkus.http.port=80 -jar target/quarkus-app/quarkus-run.jar &
