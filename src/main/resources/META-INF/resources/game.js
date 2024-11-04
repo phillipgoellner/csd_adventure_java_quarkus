837bb0c38db5f447c231f028ac6ebfd7b5110c00
Phillip Goellner <phillip.goellner@colenet.de>
2024-11-04 21:17:56 +0100

    Remove unused slice

--- src/main/resources/META-INF/resources/game.js
+++ src/main/resources/META-INF/resources/game.js
@@ -7,9 +7,6 @@ $( document ).ready(function() {
             data: {command: $('#command').val()},
         })
             .done(function( data ) {
-                if ( console && console.log ) {
-                    console.log( "Sample of data--:", data.slice( 0, 100 ) );
-                }
                 $("#gameInfo").html(data.gameInfo);
                 $('#command').val("");
                 $('#actions').html(data.actions);
