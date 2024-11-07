4586de28bf40cac19a7c2740f7f83f87936605de
Jakob Silbereisen <jakob.silbereisen@foconis.de>
2024-11-07 09:14:19 +0100

    Unit test wash hands in washroom

--- src/test/java/de/derkomischeagilist/WashroomTest.java
+++ src/test/java/de/derkomischeagilist/WashroomTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 /**
  * @author Jakob Silbereisen, Foconis Analytics GmbH
@@ -26,5 +27,13 @@ public class WashroomTest {
 		assertThat(helpText, equalTo("Try to type 'look around', 'read DoD', 'use door to hallway', 'use door to loo', "
 				+ "'put paper towels in the bin' or 'wash hands'.<br/>"
 				+ "If you want to restart, just try to 'commit suicide'."));
+	}
+
+	@Test
+	void testWashHandsStateChange() {
+		assertThat(washroom.getAreHandsWashed(), is(false));
+		String reply = washroom.handleCommand("wash hands");
+		assertThat(reply, containsStringIgnoringCase("you happily wash your hands with hot water and soap."));
+		assertThat(washroom.getAreHandsWashed(), is(true));
 	}
 }
