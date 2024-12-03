1d8e05f530a98e8aba2e102eae6cb0eb32d9c503
Muhammed Emre Akkus <hyp3rson1x@gitlab.um-a.one>
2024-12-03 11:36:05 +0100

    Add tests for teamOffice

--- src/test/java/de/derkomischeagilist/TeamOfficeTest.java
+++ src/test/java/de/derkomischeagilist/TeamOfficeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.hamcrest.Matchers.equalTo;
 
 /**
@@ -12,17 +13,33 @@ import static org.hamcrest.Matchers.equalTo;
  */
 public class TeamOfficeTest {
 
-	private TeamOffice teamOffice;
+    private TeamOffice teamOffice;
 
-	@BeforeEach
-	void init() {
-		teamOffice = new TeamOffice();
-	}
+    @BeforeEach
+    void init() {
+        teamOffice = new TeamOffice();
+    }
 
-	@Test
-	void testHelp() {
-		String helpText = teamOffice.getHelp();
-		assertThat(helpText, equalTo("Try to type 'look around' or 'use door to hallway'.<br/>"
-				+ "If you want to restart, just try to 'commit suicide'."));
-	}
+    @Test
+    void testHelp() {
+        String helpText = teamOffice.getHelp();
+        assertThat(helpText, equalTo("Try to type 'look around' or 'use door to hallway'.<br/>"
+                + "If you want to restart, just try to 'commit suicide'."));
+    }
+
+
+    @Test
+    void LookingAroundLetsYouCharts() {
+        String actualDescription = teamOffice.getDetailedDescription();
+        assertThat(actualDescription, containsStringIgnoringCase("charts"));
+    }
+
+    @Test
+    void HelpInTeamOfficeCommandToUseDoors() {
+        String teamOfficeHelp = teamOffice.getHelp();
+        String teamOfficeDescription = teamOffice.getDetailedDescription();
+        assertThat(teamOfficeDescription, containsStringIgnoringCase("look at first cubicle"));
+        assertThat(teamOfficeDescription, containsStringIgnoringCase("look at wall"));
+        assertThat(teamOfficeHelp, containsStringIgnoringCase("use door to hallway"));
+    }
 }
