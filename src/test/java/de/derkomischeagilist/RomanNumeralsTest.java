544d8272e2295fd5e9cde6cb09890b8fc191d4ea
Phillip Goellner <phillip.goellner@colenet.de>
2025-05-08 13:35:15 +0200

    TDD Kata-Setup

---
+++ src/test/java/de/derkomischeagilist/RomanNumeralsTest.java
@@ -0,0 +1,13 @@
+package de.derkomischeagilist;
+
+import org.junit.jupiter.api.Test;
+
+class RomanNumeralsTest {
+    @Test
+    void repeating_Is() {
+    }
+
+    public String toRoman(int number) {
+        return null;
+    }
+}
