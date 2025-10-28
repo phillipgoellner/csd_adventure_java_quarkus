6d0e51388fc0b0d7f45acf1e6b237c20c47decb0
Amrei Elisabeth Plaga <aplaga@psi.de>
2025-10-28 12:05:43 +0100

    fix spelling of "cold" in team office description

--- src/main/java/de/derkomischeagilist/Rooms/TeamOffice.java
+++ src/main/java/de/derkomischeagilist/Rooms/TeamOffice.java
@@ -20,7 +20,7 @@ public class TeamOffice extends AbstractRoom {
 
     @Override
     public String getDescription() {
-        return "This is a very stinky room. Smells like real work and you can feel the Cold atmosphere inside the room. \n" +
+        return "This is a very stinky room. Smells like real work and you can feel the cold atmosphere inside the room. \n" +
         		"In the middle of the room there are several dirty old cubicles. " +
                 "Nearly each one of the cubicles has a worker in front of it staring directly at the computer screen. \n" +
                 "Your smart but smelly Teammates greet you in the usual manner: 'Hey, you moron! ;)', " +
